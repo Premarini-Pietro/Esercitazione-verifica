4c40c00df28424829e0024bc0a34525dccdb4301
Premarini-Pietro <premarini.pietro@iisgalvanimi.edu.it>
2022-11-10 11:29:44 +0000

    Esercizio-2

---
+++ Esercizio-2.py
@@ -0,0 +1,8 @@
+num_1 = input("Numero-1:")
+num_2 = input("Numero-2:")
+num_3 = input("Numero-3:")
+num_4 = input("Numero-4:")
+num_5 = input("Numero-5:")
+Lista = [num_1, num_2, num_3, num_4, num_5]
+Lista.reverse()
+print("La lista è " , Lista)
