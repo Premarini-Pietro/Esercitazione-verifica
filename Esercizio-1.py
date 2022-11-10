a037d170aeec0cadb8a1e64531924f008b775ae8
Premarini-Pietro <premarini.pietro@iisgalvanimi.edu.it>
2022-11-10 11:20:01 +0000

    Esercizio_1

---
+++ Esercizio-1.py
@@ -0,0 +1,14 @@
+numero_1 = input("Numero-1:")
+print("Il numero selezionato è " + numero_1)
+
+numero_2 = input("Numero-2:")
+print("Il numero selezionato è " + numero_2)
+
+numero_3 = input("Numero-3:")
+print("Il numero selezionato è " + numero_3)
+
+numero_4 = input("Numero-4:")
+print("Il numero selezionato è " + numero_4)
+
+numero_5 = input("Numero-5:")
+print("Il numero selezionato è " + numero_5)
